6e1cb51615def24905ba7ab5c7db1c2ef8e4c050
deltamann <vincentyuct@gmail.com>
2024-03-21 23:38:37 +1300

    fix content

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,8 +5,8 @@ type Props = {}
 const content: ContentProp = {
     level: 1,
     title: "Personal Summary",
-    description: [`I have a background in software engineering and specialize in website and online application development, with more than 8 years of working experience. Currently, I am looking for roles that can help me gain more skills and experience in variousindustries, as well as improve my knowledge in the Information Technology sector.`
-    ,`I am a good listener and communicator, organized, and has proven experience in maintaining and managing business websitesand large web applications. I pay attention to small details, which aids in finding issues and solving problems. I am also open-minded to recommendations and feedback, always willing to learn and improve myself in both personal and professional aspects.`]
+    description: [`I have a background in software engineering and specialize in website and online application development, with more than 8 years of working experience. Currently, I am looking for roles that can help me gain more skills and experience in various industries, as well as improve my knowledge in the Information Technology sector.`
+    ,`I am a good listener and communicator, organized, and has proven experience in maintaining and managing business websites and large web applications. I pay attention to small details, which aids in finding issues and solving problems. I am also open-minded to recommendations and feedback, always willing to learn and improve myself in both personal and professional aspects.`]
 }
 
 const SkillList: ContentProp[] = [
