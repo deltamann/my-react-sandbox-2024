e301e6e7dfe891cf114dfed9c9caa8da4940e49f
deltamann <vincentyuct@gmail.com>
2024-05-14 22:59:25 +1200

    spacing

--- src/components/WorkHistory/WorkCard.tsx
+++ src/components/WorkHistory/WorkCard.tsx
@@ -12,32 +12,32 @@ export type WorkProp = {
 }
 
 const WorkCard = ({ detail }: { detail: WorkProp }) => {
-  return (
-    <div className="WorkCard-Wrapper">
-        <div className="WorkCard-Content">
-            <h2>{detail.title}</h2>
-            <p>
-                <span className="WorkCard-CompanyName">
-                    {
-                        detail.link 
-                            ? <a href={detail.link} rel="noreferrer" target="_blank">{detail.company}</a>
-                            : detail.company
-                    }
-                </span>&nbsp;&nbsp;
-                <span className="WorkCard-Duration">{detail.start} - {detail.finish || 'Present'}</span>
-            </p>
-            <hr />
-            <div className="WorkCard-Details">
-                <div className="WorkCard-Roles">
-                    <ContentBlock content={{level: 2, title: 'Roles:', list: detail.responsibilities}} />
-                </div>
-                <div className="WorkCard-Achievements">
-                    <ContentBlock content={{level: 2, title: 'Achievements:', list: detail.achievements}} />
+    return (
+        <div className="WorkCard-Wrapper">
+            <div className="WorkCard-Content">
+                <h2>{detail.title}</h2>
+                <p>
+                    <span className="WorkCard-CompanyName">
+                        {
+                            detail.link
+                                ? <a href={detail.link} rel="noreferrer" target="_blank">{detail.company}</a>
+                                : detail.company
+                        }
+                    </span>&nbsp;&nbsp;
+                    <span className="WorkCard-Duration">{detail.start} - {detail.finish || 'Present'}</span>
+                </p>
+                <hr />
+                <div className="WorkCard-Details">
+                    <div className="WorkCard-Roles">
+                        <ContentBlock content={{ level: 2, title: 'Roles:', list: detail.responsibilities }} />
+                    </div>
+                    <div className="WorkCard-Achievements">
+                        <ContentBlock content={{ level: 2, title: 'Achievements:', list: detail.achievements }} />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default WorkCard
